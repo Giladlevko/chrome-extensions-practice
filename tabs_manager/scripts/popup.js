6e40a654e1c5490073a22cc508c2b52568da7ffa
Gilad Levko <giladlevko@gmail.com>
2026-08-15 21:21:30 -0400

    Continued with the extension and fixed some bugs. I will extend it to godot docs in the near future!

--- tabs_manager/scripts/popup.js
+++ tabs_manager/scripts/popup.js
@@ -3,7 +3,7 @@
 const tabs = await chrome.tabs.query({
     url:[
         "https://developer.chrome.com/docs/webstore/*",
-        "https://developer.chrome.com/docs/extensions/*",
+        "https://developer.chrome.com/docs/extensions/*"
     ]
 });
 
@@ -17,17 +17,17 @@ const tabs = await chrome.tabs.query({
 const collator = new Intl.Collator();
 tabs.sort((a,b)=>collator.compare(a.title,b.title));
 
-const template = ducument.getElementById("li_template");
+const template = document.getElementById("li_template");
 const elements = new Set();
 for(const tab of tabs){
     const element = template.content.firstElementChild.cloneNode(true);
 
     const title = tab.title.split("-")[0].trim();
 
-    const path_name = new URL(tab.url).pathname.slice("/docs".length);
+    const pathname = new URL(tab.url).pathname.slice("/docs".length);
 
     element.querySelector(".title").textContent = title;
-    element.querySelector(".pathname").textContent = path_name;
+    element.querySelector(".pathname").textContent = pathname;
     element.querySelector("a").addEventListener("click",async()=>{
         //activate the tab and focus the window
         await chrome.tabs.update(tab.id,{active:true});
@@ -37,3 +37,14 @@ for(const tab of tabs){
 }
 
 document.querySelector("ul").append(...elements);
+
+
+const button = document.querySelector("button");
+button.addEventListener("click", async()=>{
+    const tabIds = tabs.map(({id})=>id);
+    if(tabIds.length){
+        const group = await chrome.tabs.group({tabIds});
+        await chrome.tabGroups.update(group,{title:"DOCS"})
+    }
+});
+
