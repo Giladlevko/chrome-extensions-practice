86440ee8ac3225d2187c2930d798af43a80d557e
Gilad Levko <giladlevko@gmail.com>
2026-08-17 23:36:12 -0400

    Continued working on the tab manager, And I made it work with thegodot docs and now it groups the codot and chrome into different groups! Next I will make an indication in the UI if its a godot tab or a chrome tab!

--- tabs_manager/scripts/popup.js
+++ tabs_manager/scripts/popup.js
@@ -3,7 +3,8 @@
 const tabs = await chrome.tabs.query({
     url:[
         "https://developer.chrome.com/docs/webstore/*",
-        "https://developer.chrome.com/docs/extensions/*"
+        "https://developer.chrome.com/docs/extensions/*",
+        "https://docs.godotengine.org/*"
     ]
 });
 
@@ -24,10 +25,15 @@ for(const tab of tabs){
 
     const title = tab.title.split("-")[0].trim();
 
-    const pathname = new URL(tab.url).pathname.slice("/docs".length);
+    let clear_path = "";
+    //removes /docs or /en/ and whatever is after it untill the next /
+    // so it removes this /en/stable for example
+    if (tab.url){
+        clear_path = new URL(tab.url).pathname.replace(/^\/(docs|en\/[^\/]+)/, "");
+    }
 
     element.querySelector(".title").textContent = title;
-    element.querySelector(".pathname").textContent = pathname;
+    element.querySelector(".pathname").textContent = clear_path;
     element.querySelector("a").addEventListener("click",async()=>{
         //activate the tab and focus the window
         await chrome.tabs.update(tab.id,{active:true});
@@ -41,10 +47,27 @@ document.querySelector("ul").append(...elements);
 
 const button = document.querySelector("button");
 button.addEventListener("click", async()=>{
-    const tabIds = tabs.map(({id})=>id);
-    if(tabIds.length){
-        const group = await chrome.tabs.group({tabIds});
-        await chrome.tabGroups.update(group,{title:"DOCS"})
+
+    const gd_ids = [];
+    const ch_ids = [];
+
+    for(const tab of tabs){
+        if(tab.url.includes("docs.godot")){
+            gd_ids.push(tab.id);
+        }
+        else if(tab.url.includes("developer.chrome")){
+            ch_ids.push(tab.id);
+        }
     }
+
+    if(gd_ids.length){
+        const group = await chrome.tabs.group({tabIds:gd_ids});
+        await chrome.tabGroups.update(group,{title:"GD-DOCS",color: "blue"});
+    }
+    if(ch_ids.length){
+        const group = await chrome.tabs.group({tabIds:ch_ids});
+        await chrome.tabGroups.update(group,{title:"CH-DOCS",color: "yellow"});
+    }
+
 });
 
